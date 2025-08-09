1833044c525afcb1c3e8075e2a04ebdb690d0d96
mochalovflute <mochalovflute@gmail.com>
2025-08-09 13:42:38 +0500

    avatars 58 08

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,22 @@
 import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCard} from './common-ui/profile-card/profile-card';
-import {Profile} from './data/services/profile';
-import {JsonPipe} from '@angular/common';
+import {ProfileService} from './data/services/profile.service';
+import {Profile} from './data/interfaces/profile.interface';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCard, JsonPipe],
+  imports: [RouterOutlet, ProfileCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   // protected readonly title = signal('tik-talk');
 
-  profileService = inject(Profile);
-  profiles: any = [];
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
 
   constructor() {
     this.profileService.getTestAccounts().subscribe(val => {
